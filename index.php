97fe18735644bb982d28c781e1729b168a3170c6
Jonathan Guzman <jguzman41@cnm.edu>
2016-10-13 16:33:03 -0600

    Social media through addthis set up

--- index.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Jonathan
- * Date: 10/11/16
- * Time: 5:12 PM
- */
---
+++ public_html/index.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+
+
+		<title> "Goose"</title>
+	</head>
+	<body>
+		<!-- this is the header-->
+		<header>
+			<h1>Albuquerque Raised, New Orleans Educated.</h1>
+		</header>
+
+		<!-- this is the main page content-->
+		<main>
+
+			<!-- begin section 1-->
+
+			<section>
+				<p>Hey, maybe you could pop a tent outside with your cousin Maeby… it'd be a good chance to rub off on her. There are dozens of us! DOZENS! I will pack your sweet pink mouth with so much ice cream you'll be the envy of every Jerry and Jane on the block! I made a huge tiny mistake. One of the guys told me to take my head out of my BOTTOM and get back to work…my BOTTOM! Hahahaha. So you take your mom to work every day? Bummer. Moms are such a pain in the ass, huh? It's, like, die already!</p>
+				<img src="img/chicken%20dance.gif" alt="chicken dance gif"
+			</section>
+
+			<!-- begin section 2-->
+			<section>
+				<h2>More Quotes!</h2>
+				<ul>
+					<li>It seems like only yesterday you were bursting forth from your mother's fertile womb.</li>
+					<li>Everyone's laughing, and riding, and cornholing except Buster.</li>
+					<li>Whenever she'd change clothes, she'd make me wait on the balcony until zip-up, and yet anything goes at bath time</li>
+				</ul>
+			</section>
+
+			<!-- begin section 3-->
+			<section>
+				<table>
+					<tr>
+						<th>Character</th>
+						<th>Quote</th>
+					</tr>
+					<tr>
+						<td>George Michael</td>
+						<td>What a fun, sexy time for you</td>
+					</tr>
+					<tr>
+						<td>Gob</td>
+						<td>Oh, yeah, the guy in the the $4,000 suit is holding the elevator for a guy who doesn't make that in three months. Come on!</td>
+					</tr>
+					</tr>
+					<tr>
+						<td>Tobias</td>
+						<td>Excuse me. Do these effectively hide my thunder?</td>
+					</tr>
+				</table>
+			</section>
+			<section> <h2> Social Media </h2> </section>
+			<!-- Go to www.addthis.com/dashboard to customize your tools --> <script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-580009aff67502f0"></script>
+		</main>
+		<!-- Go to www.addthis.com/dashboard to customize your tools --> <div class="addthis_inline_follow_toolbox"></div>
+	</body>
+</html>
