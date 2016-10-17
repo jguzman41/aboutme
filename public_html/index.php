90322292b83099d2dd7661ab247327f0b1b94de4
Jonathan Guzman <jguzman41@cnm.edu>
2016-10-17 09:40:03 -0600

    Finished timeline

--- public_html/index.php
+++ public_html/index.php
@@ -101,11 +101,11 @@
 			<section>
 				<h1>A Timeline</h1>
 					<h2>'93-'96</h2>
-					<p> Fairly uneventful. Highlights include:</p>
+					<p> Fairly uneventful.</p>
 						<ul>
 							<li>Move from the Dominican Republic to the United States.</li>
 							<li>My brother is born</li>
-							<li>I get pretty good at eating solid food</li>
+							<li>I get <i>pretty</i> good at eating solid food</li>
 						</ul>
 				<h2>'97-'00</h2>
 				<p> Memory hazy, updates include:</p>
@@ -115,7 +115,7 @@
 					<li>Give up goals to be a professional Yo-Yo'er</li>
 				</ul>
 				<h2>'01-'06</h2>
-				<p> Memory hazy, updates include:</p>
+				<p>Middle school and throwaway years</p>
 				<ul>
 					<li>Make a Myspace page</li>
 					<li>Win both English and Spanish spelling bees (career best)</li>
@@ -124,7 +124,7 @@
 				<h2>'07-'11</h2>
 				<p> #BulldogStrong</p>
 				<ul>
-					<li>Peak in highschool</li>
+					<li>Get first gray hair, contemplate mortality.</li>
 					<li>Break my foot playing soccer in the house</li>
 					<li>Join a punk band, I was the weak link.</li>
 				</ul>
@@ -135,42 +135,10 @@
 					<li>Start playing rugby, leaving my mom in a perpetual state of worry</li>
 					<li>Move to New Orleans, leaving my mom in a perpetual state of worry</li>
 				</ul>
-					<p>Hey, maybe you could pop a tent outside with your cousin Maeby… it'd be a good chance to rub off on her. There are dozens of us! DOZENS! I will pack your sweet pink mouth with so much ice cream you'll be the envy of every Jerry and Jane on the block! I made a huge tiny mistake. One of the guys told me to take my head out of my BOTTOM and get back to work…my BOTTOM! Hahahaha. So you take your mom to work every day? Bummer. Moms are such a pain in the ass, huh? It's, like, die already!</p>
 				<img src="images/ericandre-coffee.gif" alt="eric-andre gif">
 			</section>
 
-			<!-- begin section 2-->
-			<section>
-				<h2>More Quotes!</h2>
-				<ul>
-					<li>It seems like only yesterday you were bursting forth from your mother's fertile womb.</li>
-					<li>Everyone's laughing, and riding, and cornholing except Buster.</li>
-					<li>Whenever she'd change clothes, she'd make me wait on the balcony until zip-up, and yet anything goes at bath time</li>
-				</ul>
-			</section>
 
-			<!-- begin section 3-->
-			<section>
-				<table>
-					<tr>
-						<th>Character</th>
-						<th>Quote</th>
-					</tr>
-					<tr>
-						<td>George Michael</td>
-						<td>What a fun, sexy time for you</td>
-					</tr>
-					<tr>
-						<td>Gob</td>
-						<td>Oh, yeah, the guy in the the $4,000 suit is holding the elevator for a guy who doesn't make that in three months. Come on!</td>
-					</tr>
-					</tr>
-					<tr>
-						<td>Tobias</td>
-						<td>Excuse me. Do these effectively hide my thunder?</td>
-					</tr>
-				</table>
-			</section>
 			<section> <h2> Social Media </h2> 			<!-- Go to www.addthis.com/dashboard to customize your tools --> <script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-580009aff67502f0"></script>
 		</main>
 		<!-- Go to www.addthis.com/dashboard to customize your tools --> <div class="addthis_inline_follow_toolbox"></div>
